259f08562f71ed3ec4827684fc6e55a5ccab644e
Joel Onuki <oonukij@users.noreply.github.com>
2023-02-28 23:59:48 +0900

    Webhooks with variables

--- webhooks_create/webhooks_create.sh
+++ webhooks_create/webhooks_create.sh
@@ -3,40 +3,35 @@
 
 echo "*** Create Webhooks"
 
-read -p 'Project Name:' project_name
 read -p 'PAT:' pat
 
-read -p 'Area Path:' json_area_path
-read -p 'Project ID:' json_project_id
+organization_name="fernandomatsuosantos"
+project_name="DevOps PoC"
+area_path="DevOps PoC"
 
-# work_item_name="Product Backlog Item"
-# work_feature_name="Feature"
-# work_epic_name="Epic"
+# Get Project ID
+url="https://dev.azure.com/$organization_name/_apis/projects/$project_name?api-version=7.0"
+url="${url// /%20}"
+project_json=$(curl -X GET $url -H "Authorization: Basic $(echo -n "":${pat}"" | base64)")
+project_id=$(echo ${project_json} | jq '.id')
 
-# field_parent_name="Parent"
-# field_effort_name="Effort"
-# field_iteration_id_name="Iteration ID"
-# field_start_date_name="Start Date"
-# field_target_date_name="Target Date"
-# field_state_name="State"
-# field_completed_effort_name="Completed Effort"
 
-read -p 'Item Name:' work_item_name
-read -p 'Feature Name:' work_feature_name
-read -p 'Epic Name:' work_epic_name
+work_item_name="Effort"
+work_feature_name="Feature"
+work_epic_name="Epic"
 
-read -p 'Field Parent Name:' field_parent_name
-read -p 'Field Effort Name:' field_effort_name
-read -p 'Field Iteration ID Name:' field_iteration_id_name
-read -p 'Field Start Date Name:' field_start_date_name
-read -p 'Field Target Date Name:' field_target_date_name
-read -p 'Field State Name:' field_state_name
-read -p 'Field Completed Effort Name:' field_completed_effort_name
+field_parent_name="Parent"
+field_effort_name="Effort"
+field_iteration_id_name="Iteration ID"
+field_start_date_name="Start Date"
+field_target_date_name="Target Date"
+field_state_name="State"
+field_completed_effort_name="Completed Effort"
 
 # json read
 webhooks=$(cat webhooks.json)
-webhooks="${webhooks//_var_area_path_/${json_area_path}}"
-webhooks="${webhooks//_var_project_id_/${json_project_id}}"
+webhooks="${webhooks//_var_area_path_/${area_path}}"
+webhooks="${webhooks//_var_project_id_/${project_id}}"
 webhooks="${webhooks//_work_item_name_/${work_item_name}}"
 webhooks="${webhooks//_work_feature_name_/${work_feature_name}}"
 webhooks="${webhooks//_work_epic_name_/${work_epic_name}}"
@@ -49,8 +44,11 @@ webhooks="${webhooks//_field_target_date_name_/${field_target_date_name}}"
 webhooks="${webhooks//_field_state_name_/${field_state_name}}"
 webhooks="${webhooks//_field_completed_effort_name_/${field_completed_effort_name}}"
 
+webhooks="${webhooks//_organization_name_/${organization_name}}"
+
+
 for row in $(echo "${webhooks}" | jq -r '.[] | @base64'); do
     json=$(echo ${row} | base64 --decode)
-    curl -X POST "https://dev.azure.com/${project_name}/_apis/hooks/subscriptions?api-version=6.0" -H "Authorization: Basic $(echo -n "":${pat}"" | base64)" -H 'Content-Type: application/json' -d "$json"
+    curl -X POST "https://dev.azure.com/${organization_name}/_apis/hooks/subscriptions?api-version=6.0" -H "Authorization: Basic $(echo -n "":${pat}"" | base64)" -H 'Content-Type: application/json' -d "$json"
 done
 
