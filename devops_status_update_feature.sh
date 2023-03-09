58653a3419b5f07cf7502ba879971387787d1921
Fernando Matsuo Santos <fernando@familiamatsuo.com>
2023-03-09 18:12:30 -0300

    add more options to status automation

--- devops_status_update_feature.sh
+++ devops_status_update_feature.sh
@@ -67,18 +67,23 @@ for row_backlog in $(echo "${backlogs}" | jq -r '.[] | @base64'); do
 
     if [[ " ${states_1_one[*]} " =~ " $state " ]]; then
         state_final="${states_1_one_set_status}"
+        $state_all_same == 0
         break
     elif [[ " ${states_2_one[*]} " =~ " $state " ]]; then
         state_final="${states_2_one_set_status}"
+        $state_all_same == 0
         break
     elif [[ " ${states_3_one[*]} " =~ " $state " ]]; then
         state_final="${states_3_one_set_status}"
+        $state_all_same == 0
         break
     elif [[ " ${states_4_one[*]} " =~ " $state " ]]; then
         state_final="${states_4_one_set_status}"
+        $state_all_same == 0
         break
     elif [[ " ${states_5_one[*]} " =~ " $state " ]]; then
         state_final="${states_5_one_set_status}"
+        $state_all_same == 0
         break
     fi
 
